40c043c7d5abdeb22fc1dfd4158e701da8b4cfe6
dareenakkila789 <dalefa08@gmail.com>
2020-01-03 09:27:40 +0200

    login and signup pages

--- src/components/signup.js
+++ src/components/signup.js
@@ -23,7 +23,7 @@ class login extends Component{
           <img className="logo" src ={weblogo} alt= 'logo'/> </div>
                 <h1>Let's create your account!</h1>
                <div className ='firstinput'><input className='navbar' value={text} placeholder="enter ur email"  onChange={handleChange}/></div> 
-               <div className ='secondinput'> <input className='navbar' value={text} placeholder="enter ur passoword" onChange={handleChange}/></div>
+               <div className ='secondinput'> <input className='navbar' type='password' value={text} placeholder="enter ur passoword" onChange={handleChange}/></div>
                 <button className='navbutton'>create</button>
                 
             </div>
