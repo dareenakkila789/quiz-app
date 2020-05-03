a95a7093073f38f1a742220f1679d95e47e66697
dareenakkila789 <dalefa08@gmail.com>
2020-05-03 17:49:52 +0300

    done!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
 import Login from "./components/login";
-import didntwork from "./components/didntWork";
+
 // import weblogo from "../weblogo.png";
 import SignUp from "./components/signup";
 import error from "./components/error";
-import test2 from "./components/test2";
+
 import display from "./components/display";
 import quiz from "./components/quiz";
 import home from "./components/mainpage";
@@ -27,12 +27,12 @@ class App extends Component {
             <Switch>
               <Route exact path="/" component={AppBar} />
               <Route exact path="/login" component={Login} />
-              <Route exact path="/work" component={didntwork} />
+
               <Route exact path="/first" component={first} />
               <Route exact path="/signup" component={SignUp} />
               {/* <Route exact path="/test" component={test} /> */}
               <Route exact path="/choose" component={choose} />
-              <Route exact path="/try" component={test2} />
+
               <Route exact path="/display" component={display} />
               <Route exact path="/home" component={home} />
               <Route exact path="/quiz" component={quiz} />
